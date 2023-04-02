f99bdb0e3096da48e2b9939d589c5c07b50c277e
Richard <rfazzio@fau.edu>
2023-04-02 16:58:31 -0400

    Initial commit

---
+++ constants/messages.js
@@ -0,0 +1,16 @@
+export const MESSAGE_TEXT = {
+	NO_COMMAND_FOUND: 'That command is not supported current :(',
+	UNKNOWN_ERROR: 'Oops! Unknowned error occurred sorry.',
+	TIMER_STARTED: 'Countdown begun with %t minutes left.',
+	TIMER_CLEARED: 'Countdown ended.',
+	TIMER_NOT_FOUND: 'Oops, no countdown was found for you.',
+	TIMER_ENDED: 'The countdown has ended.',
+	TIMER_MINUTE_REMAINING: 'There is 1 minute ramining in the countdown',
+	TIMER_MINUTES_REMAINING: 'There are %t minutes ramining in the countdown',
+	NUMBER_REQUIRED:
+		'A number is required as one of the parameters for this command.',
+	TIMER_TOO_LARGE:
+		'The maximum duration supported by this command is %max minutes. %time was provided, please use %max minutes or less.',
+	TIMER_ALREADY_EXISTS:
+		'A countdown for this user is already being handled, please use !stop command to stop the previous countdown or wait for the previous command to finish.',
+};

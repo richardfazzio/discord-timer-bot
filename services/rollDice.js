bd62234f48b3cc7d30088c526015b90b98662e96
richardfazzio <rfazziodev@gmail.com>
2024-01-24 20:11:18 -0500

    Fix regex.

--- services/rollDice.js
+++ services/rollDice.js
@@ -5,12 +5,13 @@ const ROLL_REPLACE_TEXT = '%roll!'
 
 const DEFAULT_ROLL = /^!roll\s*$/i
 const ROLL_REMOVE = /^!roll\s*/i
-const N_ROLL = /^!roll\s*$/i
-const D_ROLL = /^!roll\s+\d+$/i
+const N_ROLL = /^!roll\s*\d+$/i
+const D_ROLL = /^!roll\s+\d+d\d+$/i
 
 export function handleRoll(msg) {
     try {
-        const {content} = msg;
+        const {content} = msg
+        console.log(content, msg)
         if (content.match(DEFAULT_ROLL)) {
             return createResponseText(rollDice());
         }
@@ -19,19 +20,19 @@ export function handleRoll(msg) {
             return createResponseText(rollDice(n));
         }
         if (content.match(D_ROLL)) {
-            const dString = +removeRollText(content)
+            const dString = removeRollText(content)
             const [numberOfDice, diceSize] = dString.split(/d/i)
-            return createResponseText(rollDiceNTimes(numberOfDice, diceSize));
+            return createResponseText(rollDiceNTimes(+numberOfDice, +diceSize));
         }
         throw new Error('Unknown dice roll commant')
     } catch (error) {
         console.error('Failed to handle roll', { content: msg.content, error: JSON.stringify(error), message: 'Failed to roll dice', errorMessage: error.message });
-        return UNKNOWN_ERROR;
+        return MESSAGE_TEXT.UNKNOWN_ERROR;
     }
 }
 
-function removeRollText(msg) {
-    return msg.content.replaceAll(ROLL_REMOVE, '');
+function removeRollText(content) {
+    return content.replace(ROLL_REMOVE, '');
 }
 
 function rollDiceNTimes(numberOfDice, diceSize) {
